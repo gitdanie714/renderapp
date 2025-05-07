518c23cde91b9ec40c8fe23409d17cd801ff1a86
Orji Daniella Amarachi <danielamarachi20@gmail.com>
2025-05-07 05:58:38 -0700

    First Commit

---
+++ src/main/java/com/elladev/renderapp/controller/RenderController.java
@@ -0,0 +1,13 @@
+package com.elladev.renderapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RenderController {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello from render based springboot app";
+    }
+}
---
+++ src/test/java/com/elladev/renderapp/RenderappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elladev.renderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
